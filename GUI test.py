2f12f123379d4feb64f34e6304c29c0632d9ce79
TheRealWushuMaster <chiossi@gmail.com>
2023-05-19 18:00:33 -0300

    Minor modification

--- GUI test.py
+++ GUI test.py
@@ -13,6 +13,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                run = False
     
     pygame.display.update()
 
