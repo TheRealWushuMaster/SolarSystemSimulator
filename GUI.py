86ed73a3bc70a6d107d33586fbf39e1983629d2a
TheRealWushuMaster <chiossi@gmail.com>
2023-06-30 17:00:50 -0300

    Some more work

--- GUI.py
+++ GUI.py
@@ -1,11 +1,12 @@
 from settings import *
-#import tkinter as tk
 import customtkinter as ctk
 from skyfield.api import load, Topos
 import requests
 import os
 import email.utils
 import classes
+from jplephem.spk import SPK
+from jplephem import Ephemeris
 
 class App(ctk.CTk):
     def __init__(self):
@@ -16,16 +17,19 @@ class App(ctk.CTk):
         self.minsize(WINDOW_MIN_SIZE[0], WINDOW_MIN_SIZE[1])
         self.iconbitmap("logos/logo-2.ico")
         self.configure(fg_color = DEFAULT_BACKGROUND)
-        
-        self.check_update()
-
-        Controls(self)
-        
-        self.create_bodies()
-
+        # Verify ephemeris updates
+        self.check_ephemeris_file_update()
+        self.load_ephemeris_data()
+        # Create widgets
+        Widgets(self)
+        # Create celestial bodies        
+        celestial_bodies = self.create_bodies()
+        # Draw celestial bodies
+        self.draw_celestial_bodies(celestial_bodies, Widgets.canvas)
+        # Start main loop
         self.mainloop()
 
-    def check_update(self):
+    def check_ephemeris_file_update(self):
         if os.path.exists(ephemeris_file):
             current_timestamp = os.path.getmtime(ephemeris_file)
         else:
@@ -46,26 +50,46 @@ class App(ctk.CTk):
             self.download_updated_ephemeris()
         else:
             print("Ephemeris file is up to date.")
-    
+
     def download_updated_ephemeris(self):
         response = requests.get(ephemeris_url)
         with open(ephemeris_file, "wb") as file:
             file.write(response.content)
-    
-    def create_bodies(self):
-        pass
-        # sun = classes.Star(SUN_STAR_TYPE, 1, "Sun", 0, 0, SUN_RADIUS, SUN_MASS, SUN_TEMPERATURE,
-        #                    SUN_ROTATIONAL_PERIOD, SUN_COLOR, SUN_TEXTURE)
-        # mercury = classes.Planet(sun)
-        # venus = classes.Planet(sun, 1)
-        # earth = classes.Planet(sun, 1)
 
-class Controls(ctk.CTkFrame):
+    def load_ephemeris_data(self):
+        kernel = SPK.open('de421.bsp')
+        eph = Ephemeris(de421='de421.bsp')
+        return kernel
+    
+    def close_ephemeris_data(self, kernel):
+        kernel.close()
+
+    def create_bodies(self):
+        celestial_bodies = {}
+        # Create stars
+        for star_name, star_props in Star_data.items():
+            star = classes.Star(**star_props)
+            celestial_bodies[star_name] = star
+        # Create planets
+        for planet_name, planet_properties in Planet_data.items():
+            parent_body = celestial_bodies[planet_properties['PARENT_BODY']]  # Get the parent body object
+            planet = classes.Planet(parent_body=parent_body, **planet_properties)
+            celestial_bodies[planet_name] = planet
+        # Create moons
+        for moon_name, moon_properties in Moon_data.items():
+            parent_body = celestial_bodies[moon_properties['PARENT_BODY']]  # Get the parent body object
+            moon = classes.Planet(parent_body=parent_body, **moon_properties)
+            celestial_bodies[moon_name] = moon
+        return celestial_bodies
+
+    def draw_celestial_bodies(self, celestial_bodies, canvas):
+        pass
+
+class Widgets(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(master=parent)
         self.configure(fg_color=DEFAULT_BACKGROUND)
-        canvas = ctk.CTkCanvas(parent, bg="black")
-
-        canvas.pack(fill="both", expand=True)
+        self.canvas = ctk.CTkCanvas(parent, bg="black")
+        self.canvas.pack(fill="both", expand=True)
 
 App()
